98fd409dd1883a2d1f7a83a79268ba12e86ed892
Junye <jijunye1@outlook.com>
2026-03-12 18:26:58 -0700

    refactor(Matrix/Elementary): Tidy proofs and simplify lemmas

--- NormalForms/Matrix/Elementary/Basic.lean
+++ NormalForms/Matrix/Elementary/Basic.lean
@@ -96,62 +96,65 @@ theorem replayLog_append {m n R : Type _} [DecidableEq m] [DecidableEq n] [Add R
   | cons step rest ih =>
       simp [ih]
 
-theorem basisRow_vecMul {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (i : m) :
+theorem basisRow_vecMul {m n R : Type _} [Fintype m] [DecidableEq m] [CommRing R]
+    (A : _root_.Matrix m n R) (i : m) :
     _root_.Matrix.vecMul ((1 : _root_.Matrix m m R) i) A = A i := by
   ext j
   rw [_root_.Matrix.vecMul, dotProduct, Finset.sum_eq_single i]
   · simp
   · intro x _ hx
     have hix : i ≠ x := by simpa [eq_comm] using hx
-    simp [_root_.Matrix.one_apply, hix]
+    simp [hix]
   · intro hi
     exact (hi (Finset.mem_univ i)).elim
 
-theorem basisRow_smul_vecMul {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (i : m) (c : R) :
+theorem basisRow_smul_vecMul {m n R : Type _} [Fintype m] [DecidableEq m] [CommRing R]
+    (A : _root_.Matrix m n R) (i : m) (c : R) :
     _root_.Matrix.vecMul (c • (1 : _root_.Matrix m m R) i) A = c • A i := by
   ext j
   rw [_root_.Matrix.vecMul, dotProduct, Finset.sum_eq_single i]
   · simp [Pi.smul_apply, smul_eq_mul]
   · intro x _ hx
     have hix : i ≠ x := by simpa [eq_comm] using hx
-    simp [Pi.smul_apply, smul_eq_mul, _root_.Matrix.one_apply, hix]
+    simp [Pi.smul_apply, smul_eq_mul, hix]
   · intro hi
     exact (hi (Finset.mem_univ i)).elim
 
-theorem basisRow_add_smul_vecMul {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n]
-    [DecidableEq n] [CommRing R] (A : _root_.Matrix m n R) (src dst : m) (c : R) :
-    _root_.Matrix.vecMul (((1 : _root_.Matrix m m R) dst) + c • (1 : _root_.Matrix m m R) src) A =
+theorem basisRow_add_smul_vecMul {m n R : Type _} [Fintype m] [DecidableEq m]
+    [CommRing R] (A : _root_.Matrix m n R) (src dst : m) (c : R) :
+    _root_.Matrix.vecMul
+        (((1 : _root_.Matrix m m R) dst) + c • (1 : _root_.Matrix m m R) src) A =
       A dst + c • A src := by
   rw [_root_.Matrix.add_vecMul, _root_.Matrix.smul_vecMul]
   rw [basisRow_vecMul, basisRow_vecMul]
 
-theorem mulVec_basisCol {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (j : n) :
+theorem mulVec_basisCol {m n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
+    (A : _root_.Matrix m n R) (j : n) :
     _root_.Matrix.mulVec A (fun i => (1 : _root_.Matrix n n R) i j) = fun i => A i j := by
   ext i
   rw [_root_.Matrix.mulVec, dotProduct, Finset.sum_eq_single j]
   · simp
   · intro x _ hx
-    simp [_root_.Matrix.one_apply, hx]
+    simp [hx]
   · intro hj
     exact (hj (Finset.mem_univ j)).elim
 
-theorem mulVec_smul_basisCol {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n]
-    [DecidableEq n] [CommRing R] (A : _root_.Matrix m n R) (j : n) (c : R) :
+theorem mulVec_smul_basisCol {m n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
+    (A : _root_.Matrix m n R) (j : n) (c : R) :
     _root_.Matrix.mulVec A (c • fun i => (1 : _root_.Matrix n n R) i j) = fun i => c * A i j := by
   ext i
   rw [_root_.Matrix.mulVec, dotProduct, Finset.sum_eq_single j]
-  · simp [Pi.smul_apply, smul_eq_mul, mul_comm, mul_left_comm, mul_assoc]
+  · simp [Pi.smul_apply, smul_eq_mul, mul_comm]
   · intro x _ hx
-    simp [Pi.smul_apply, smul_eq_mul, _root_.Matrix.one_apply, hx]
+    simp [Pi.smul_apply, smul_eq_mul, hx]
   · intro hj
     exact (hj (Finset.mem_univ j)).elim
 
-theorem mulVec_add_smul_basisCol {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n]
-    [DecidableEq n] [CommRing R] (A : _root_.Matrix m n R) (src dst : n) (c : R) :
-    _root_.Matrix.mulVec A ((fun i => (1 : _root_.Matrix n n R) i dst) + c • fun i => (1 : _root_.Matrix n n R) i src) =
+theorem mulVec_add_smul_basisCol {m n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
+    (A : _root_.Matrix m n R) (src dst : n) (c : R) :
+    _root_.Matrix.mulVec A
+        ((fun i => (1 : _root_.Matrix n n R) i dst) +
+          c • fun i => (1 : _root_.Matrix n n R) i src) =
       fun i => A i dst + c * A i src := by
   rw [_root_.Matrix.mulVec_add, _root_.Matrix.mulVec_smul, mulVec_basisCol, mulVec_basisCol]
   ext i
@@ -165,14 +168,14 @@ def columnScaleMatrix {n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
     (j : n) (c : R) : _root_.Matrix n n R :=
   _root_.Matrix.updateCol (1 : _root_.Matrix n n R) j (c • fun i => (1 : _root_.Matrix n n R) i j)
 
-theorem rowScaleMatrix_mul {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (i : m) (c : R) :
+theorem rowScaleMatrix_mul {m n R : Type _} [Fintype m] [DecidableEq m] [CommRing R]
+    (A : _root_.Matrix m n R) (i : m) (c : R) :
     rowScaleMatrix i c * A = _root_.Matrix.updateRow A i (c • A i) := by
   unfold rowScaleMatrix
   rw [_root_.Matrix.updateRow_mul, _root_.Matrix.one_mul, basisRow_smul_vecMul]
 
-theorem mul_columnScaleMatrix {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (j : n) (c : R) :
+theorem mul_columnScaleMatrix {m n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
+    (A : _root_.Matrix m n R) (j : n) (c : R) :
     A * columnScaleMatrix j c = _root_.Matrix.updateCol A j (fun i => c * A i j) := by
   unfold columnScaleMatrix
   rw [_root_.Matrix.mul_updateCol, _root_.Matrix.mul_one, mulVec_smul_basisCol]
@@ -207,20 +210,18 @@ def columnOperationMatrix {n R : Type _} [Fintype n] [DecidableEq n] [CommRing R
         ((fun i => (1 : _root_.Matrix n n R) i dst) + c • fun i => (1 : _root_.Matrix n n R) i src)
   | .smul i c => columnScaleMatrix i c
 
-theorem rowOperationMatrix_mul {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (op : RowOperation R m) :
+theorem rowOperationMatrix_mul {m n R : Type _} [Fintype m] [DecidableEq m] [CommRing R]
+    (A : _root_.Matrix m n R) (op : RowOperation R m) :
     rowOperationMatrix op * A = applyRowOperation A op := by
   cases op with
   | swap i j =>
       ext r col
       by_cases hr₁ : r = i
       · subst hr₁
-        simpa [rowOperationMatrix, applyRowOperation] using
-          (_root_.Matrix.swap_mul_apply_left (R := R) (i := i) (j := j) (a := col) (g := A))
+        simp [rowOperationMatrix, applyRowOperation]
       · by_cases hr₂ : r = j
         · subst hr₂
-          simpa [rowOperationMatrix, applyRowOperation, hr₁] using
-            (_root_.Matrix.swap_mul_apply_right (R := R) (i := i) (j := j) (a := col) (g := A))
+          simp [rowOperationMatrix, applyRowOperation, hr₁]
         · simpa [rowOperationMatrix, applyRowOperation, hr₁, hr₂] using
             (_root_.Matrix.swap_mul_of_ne (R := R) (i := i) (j := j) (a := r) (b := col) hr₁ hr₂ A)
   | add src dst c =>
@@ -232,27 +233,25 @@ theorem rowOperationMatrix_mul {m n R : Type _} [Fintype m] [DecidableEq m] [Fin
         simp [applyRowOperation, _root_.Matrix.updateRow_apply, smul_eq_mul]
       · simp [applyRowOperation, _root_.Matrix.updateRow_apply, hr]
   | smul i c =>
-      simp [rowOperationMatrix, rowScaleMatrix_mul]
+      rw [rowOperationMatrix, rowScaleMatrix_mul]
       ext r col
       by_cases hr : r = i
       · subst hr
         simp [applyRowOperation, _root_.Matrix.updateRow_apply, smul_eq_mul]
       · simp [applyRowOperation, _root_.Matrix.updateRow_apply, hr]
 
-theorem mul_columnOperationMatrix {m n R : Type _} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n]
-    [CommRing R] (A : _root_.Matrix m n R) (op : ColumnOperation R n) :
+theorem mul_columnOperationMatrix {m n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
+    (A : _root_.Matrix m n R) (op : ColumnOperation R n) :
     A * columnOperationMatrix op = applyColumnOperation A op := by
   cases op with
   | swap i j =>
       ext r col
       by_cases hc₁ : col = i
       · subst hc₁
-        simpa [columnOperationMatrix, applyColumnOperation] using
-          (_root_.Matrix.mul_swap_apply_left (R := R) (i := i) (j := j) (a := r) (g := A))
+        simp [columnOperationMatrix, applyColumnOperation]
       · by_cases hc₂ : col = j
         · subst hc₂
-          simpa [columnOperationMatrix, applyColumnOperation, hc₁] using
-            (_root_.Matrix.mul_swap_apply_right (R := R) (i := i) (j := j) (a := r) (g := A))
+          simp [columnOperationMatrix, applyColumnOperation, hc₁]
         · simpa [columnOperationMatrix, applyColumnOperation, hc₁, hc₂] using
             (_root_.Matrix.mul_swap_of_ne (R := R) (i := i) (j := j) (a := r) (b := col) hc₁ hc₂ A)
   | add src dst c =>
@@ -261,29 +260,32 @@ theorem mul_columnOperationMatrix {m n R : Type _} [Fintype m] [DecidableEq m] [
       ext r col
       by_cases hc : col = dst
       · subst hc
-        simp [applyColumnOperation, _root_.Matrix.updateCol_apply]
-      · simp [applyColumnOperation, _root_.Matrix.updateCol_apply, hc]
+        simp [applyColumnOperation]
+      · simp [applyColumnOperation, hc]
   | smul i c =>
-      simp [columnOperationMatrix, mul_columnScaleMatrix]
+      rw [columnOperationMatrix, mul_columnScaleMatrix]
       ext r col
       by_cases hc : col = i
       · subst hc
-        simp [applyColumnOperation, _root_.Matrix.updateCol_apply]
-      · simp [applyColumnOperation, _root_.Matrix.updateCol_apply, hc]
+        simp [applyColumnOperation]
+      · simp [applyColumnOperation, hc]
 
 theorem det_rowAddMatrix_of_ne {m R : Type _} [Fintype m] [DecidableEq m] [CommRing R]
     (src dst : m) (c : R) (h : src ≠ dst) :
     (rowOperationMatrix (.add src dst c : RowOperation R m)).det = 1 := by
   unfold rowOperationMatrix
-  rw [_root_.Matrix.det_updateRow_add_smul_self (A := (1 : _root_.Matrix m m R)) (i := dst) (j := src)
-    h.symm c, _root_.Matrix.det_one]
+  rw [
+    _root_.Matrix.det_updateRow_add_smul_self
+      (A := (1 : _root_.Matrix m m R)) (i := dst) (j := src) h.symm c,
+    _root_.Matrix.det_one
+  ]
 
 theorem det_columnAddMatrix_of_ne {n R : Type _} [Fintype n] [DecidableEq n] [CommRing R]
     (src dst : n) (c : R) (h : src ≠ dst) :
     (columnOperationMatrix (.add src dst c : ColumnOperation R n)).det = 1 := by
   simpa [columnOperationMatrix, _root_.Matrix.det_one] using
-    (_root_.Matrix.det_updateCol_add_smul_self (A := (1 : _root_.Matrix n n R)) (i := dst) (j := src)
-      h.symm c)
+    (_root_.Matrix.det_updateCol_add_smul_self
+      (A := (1 : _root_.Matrix n n R)) (i := dst) (j := src) h.symm c)
 
 def UnimodularRowOperation {m R : Type _} [CommRing R] : RowOperation R m -> Prop
   | .swap _ _ => True
@@ -333,15 +335,18 @@ theorem replayLog_eq_left_right {m n R : Type _} [Fintype m] [DecidableEq m] [Fi
                 = leftAccumulator rest * (rowOperationMatrix op * A) * rightAccumulator rest := by
                     simp [leftAccumulator, rightAccumulator, Matrix.mul_assoc]
             _ = replayLog (rowOperationMatrix op * A) rest := ih _
-            _ = replayLog (applyStep A (.row op)) rest := by simp [applyStep, rowOperationMatrix_mul]
+            _ = replayLog (applyStep A (.row op)) rest := by
+                  simp [applyStep, rowOperationMatrix_mul]
             _ = replayLog A (.row op :: rest) := by simp [replayLog_cons]
       | col op =>
           calc
             leftAccumulator (.col op :: rest) * A * rightAccumulator (.col op :: rest)
-                = leftAccumulator rest * (A * columnOperationMatrix op) * rightAccumulator rest := by
+                = leftAccumulator rest *
+                    (A * columnOperationMatrix op) * rightAccumulator rest := by
                     simp [leftAccumulator, rightAccumulator, Matrix.mul_assoc]
             _ = replayLog (A * columnOperationMatrix op) rest := ih _
-            _ = replayLog (applyStep A (.col op)) rest := by simp [applyStep, mul_columnOperationMatrix]
+            _ = replayLog (applyStep A (.col op)) rest := by
+                  simp [applyStep, mul_columnOperationMatrix]
             _ = replayLog A (.col op :: rest) := by simp [replayLog_cons]
 
 def bezoutCoreMatrix {R : Type _} [EuclideanDomain R] [DecidableEq R] (a b : R) :
@@ -353,25 +358,35 @@ def bezoutReductionMatrix {R : Type _} [EuclideanDomain R] [DecidableEq R] (a b 
     _root_.Matrix (Fin 2) (Fin 2) R :=
   if EuclideanDomain.gcd a b = 0 then 1 else bezoutCoreMatrix a b
 
-theorem bezoutReductionMatrix_mulVec {R : Type _} [EuclideanDomain R] [DecidableEq R] (a b : R) :
+theorem bezoutReductionMatrix_mulVec {R : Type _} [EuclideanDomain R] [DecidableEq R]
+    (a b : R) :
     _root_.Matrix.mulVec (bezoutReductionMatrix a b) ![a, b] = ![EuclideanDomain.gcd a b, 0] := by
   by_cases hg : EuclideanDomain.gcd a b = 0
   · rcases EuclideanDomain.gcd_eq_zero_iff.mp hg with ⟨ha, hb⟩
     rw [bezoutReductionMatrix, if_pos hg]
-    ext i <;> fin_cases i <;> simp [ha, hb]
+    ext i
+    fin_cases i
+    · simp [ha, hb]
+    · simp [ha, hb]
   · rw [bezoutReductionMatrix, if_neg hg]
-    ext i <;> fin_cases i
-    · simp [bezoutCoreMatrix, _root_.Matrix.mulVec, dotProduct, Fin.sum_univ_two,
-        EuclideanDomain.gcd_eq_gcd_ab, add_comm, add_left_comm, add_assoc,
-        mul_comm, mul_left_comm, mul_assoc]
-    · simp [bezoutCoreMatrix, _root_.Matrix.mulVec, dotProduct, Fin.sum_univ_two]
+    ext i
+    fin_cases i
+    · simpa [bezoutCoreMatrix, _root_.Matrix.mulVec, dotProduct, Fin.sum_univ_two, mul_comm] using
+        (EuclideanDomain.gcd_eq_gcd_ab a b).symm
+    · suffices h :
+          -(b / EuclideanDomain.gcd a b) * a + (a / EuclideanDomain.gcd a b) * b = 0 by
+        simpa [bezoutCoreMatrix, _root_.Matrix.mulVec, dotProduct, Fin.sum_univ_two] using h
       apply (mul_left_cancel₀ hg)
-      rw [mul_zero, mul_add, mul_neg]
+      rw [mul_zero]
       calc
-        -(EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b * a)) +
-            EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b * b)
-            = -((EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b)) * a) +
-                (EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b)) * b := by ring
+        EuclideanDomain.gcd a b *
+            (-(b / EuclideanDomain.gcd a b) * a + (a / EuclideanDomain.gcd a b) * b) =
+          -(EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b * a)) +
+            EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b * b) := by
+              ring
+        _ = -((EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b)) * a) +
+              (EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b)) * b := by
+              ring
         _ = -(b * a) + a * b := by
               rw [EuclideanDomain.mul_div_cancel' hg (EuclideanDomain.gcd_dvd_right a b),
                 EuclideanDomain.mul_div_cancel' hg (EuclideanDomain.gcd_dvd_left a b)]
@@ -379,7 +394,8 @@ theorem bezoutReductionMatrix_mulVec {R : Type _} [EuclideanDomain R] [Decidable
 
 theorem vecMul_bezoutReductionMatrix_transpose {R : Type _} [EuclideanDomain R] [DecidableEq R]
     (a b : R) :
-    _root_.Matrix.vecMul ![a, b] (bezoutReductionMatrix a b).transpose = ![EuclideanDomain.gcd a b, 0] := by
+    _root_.Matrix.vecMul ![a, b] (bezoutReductionMatrix a b).transpose =
+      ![EuclideanDomain.gcd a b, 0] := by
   simpa using
     ((_root_.Matrix.vecMul_transpose (bezoutReductionMatrix a b) ![a, b]).trans
       (bezoutReductionMatrix_mulVec a b))
@@ -388,24 +404,27 @@ theorem det_bezoutReductionMatrix {R : Type _} [EuclideanDomain R] [DecidableEq 
     (bezoutReductionMatrix a b).det = 1 := by
   by_cases hg : EuclideanDomain.gcd a b = 0
   · simp [bezoutReductionMatrix, hg]
-  · simp [bezoutReductionMatrix, hg, bezoutCoreMatrix, _root_.Matrix.det_fin_two_of]
+  · suffices hdet :
+        EuclideanDomain.gcdA a b * (a / EuclideanDomain.gcd a b) +
+          EuclideanDomain.gcdB a b * (b / EuclideanDomain.gcd a b) = 1 by
+      simpa [bezoutReductionMatrix, hg, bezoutCoreMatrix, _root_.Matrix.det_fin_two_of,
+        sub_eq_add_neg] using hdet
     apply (mul_left_cancel₀ hg)
     rw [mul_one]
     calc
       EuclideanDomain.gcd a b *
           (EuclideanDomain.gcdA a b * (a / EuclideanDomain.gcd a b) +
-            EuclideanDomain.gcdB a b * (b / EuclideanDomain.gcd a b))
-          = EuclideanDomain.gcdA a b *
-              (EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b)) +
-              EuclideanDomain.gcdB a b *
-                (EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b)) := by ring
+            EuclideanDomain.gcdB a b * (b / EuclideanDomain.gcd a b)) =
+        EuclideanDomain.gcdA a b *
+            (EuclideanDomain.gcd a b * (a / EuclideanDomain.gcd a b)) +
+          EuclideanDomain.gcdB a b *
+            (EuclideanDomain.gcd a b * (b / EuclideanDomain.gcd a b)) := by
+            ring
       _ = EuclideanDomain.gcdA a b * a + EuclideanDomain.gcdB a b * b := by
             rw [EuclideanDomain.mul_div_cancel' hg (EuclideanDomain.gcd_dvd_left a b),
               EuclideanDomain.mul_div_cancel' hg (EuclideanDomain.gcd_dvd_right a b)]
       _ = EuclideanDomain.gcd a b := by
-            simpa [EuclideanDomain.gcd_eq_gcd_ab, add_assoc, add_left_comm, add_comm,
-              mul_assoc, mul_left_comm, mul_comm]
-
+            simp [EuclideanDomain.gcd_eq_gcd_ab, mul_comm]
 end NormalForms.Matrix.Elementary
 
 
